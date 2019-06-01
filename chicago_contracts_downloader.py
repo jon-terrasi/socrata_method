434820cc27717294df7a44ee8284b173e595b148
Jonathan Terrasi <jonathanterrasi@gmail.com>
2019-06-01 15:26:50 -0500

    Changed variable names to more intuitive/sensible conventions, removed commented-out test lines.

--- chicago_contracts_downloader.py
+++ chicago_contracts_downloader.py
@@ -11,18 +11,15 @@ client = Socrata("data.cityofchicago.org", None)
 
 # must set query_int to at least dataset size or only 1000 records returned
 query_int = 200000
-results = client.get("b6tt-rgti", limit=query_int)
+query = client.get("b6tt-rgti", limit=query_int)
 
 # [see sodapy .get() for more useful options]
 
 # convert to DataFrame object
-results_df = pd.DataFrame.from_records(results)
-
-# change NaN to 0
-#results_df = results_df.fillna(0)
+query_df = pd.DataFrame.from_records(query)
 
 # export DataFrame to CSV file
-results_df.to_csv(r'now.csv')
+query_df.to_csv(r'current.csv')
 
 # create timestamp for file naming
 ts = datetime.datetime.now().strftime('%Y-%m-%d_%H:%M:%S')
@@ -46,60 +43,60 @@ for entry in files_list:
 # in case of no pre-existing database, download current copy and exit
 if len(csv_list) == 0 and len(txt_list) == 0:
     print('No pre-existing CSV databases or hash files found.')
-    now_csv = open('now.csv', 'r')
-    now = now_csv.readlines()
-    now_hash = []
-    for line in now:
+    current_csv = open('current.csv', 'r')
+    current = current_csv.readlines()
+    current_hash = []
+    for line in current:
         result = hashlib.md5(line.encode()).digest()
-        now_hash.append(result)
+        current_hash.append(result)
     f = open(ts + '_hash.txt', 'w')
-    for element in now_hash:
+    for element in current_hash:
         f.write("%s\n" % element)
     f.close()
-    now_csv.close()
-    os.rename('now.csv', ts + '_contracts.csv')
+    current_csv.close()
+    os.rename('current.csv', ts + '_contracts.csv')
     quit()
 
-# sort by modify time and save latest to "prev_csv"
+# sort by modify time and save latest to "previous_csv"
 txt_list = sorted(txt_list, key=os.path.getmtime)
-prev_txt = txt_list[-1]
+previous_txt = txt_list[-1]
 
 # limit total number of CSV files to archive_int 
 archive_int = 10
 if len(csv_list) > archive_int:
-    print('More than', archive_int, 'copies of prev database.')
+    print('More than', archive_int, 'copies of previous database.')
     os.remove(csv_list[0:-(archive_int + 1)])
 
-# open previous txt hash file, create txt hash file for now.csv
-prev_hashfile = open(prev_txt, 'r')
-now_csv = open('now.csv', 'r')
+# open previous txt hash file, create txt hash file for current.csv
+previous_hashfile = open(previous_txt, 'r')
+current_csv = open('current.csv', 'r')
 
-prev = prev_hashfile.readlines()
-now = now_csv.readlines()
+previous = previous_hashfile.readlines()
+current = current_csv.readlines()
 
-prev_hash = []
-for line in prev:
-    prev_hash.append(line)
+previous_hash = []
+for line in previous:
+    previous_hash.append(line)
 
-now_hash = []
-for line in now:
+current_hash = []
+for line in current:
     result = hashlib.md5(line.encode()).digest()
-    now_hash.append(result)
+    current_hash.append(result)
 
-# compare "now" to "prev" in reverse order, break loop at first new line 
-for line in reversed(now_hash):
-    if line not in reversed(prev_hash):
+# compare "current" to "previous" in reverse order, break loop first new line 
+for line in reversed(current_hash):
+    if line not in reversed(previous_hash):
         print('Chicago Contracts database updated since last execution.')
-        now_csv.close()
+        current_csv.close()
         f = open(ts + '_hash.txt', 'w')
-        for element in now_hash:
+        for element in current_hash:
             f.write("%s\n" % element)
         f.close()
-        prev_hashfile.close()
-        os.rename('now.csv', ts + '_contracts.csv')
+        previous_hashfile.close()
+        os.rename('current.csv', ts + '_contracts.csv')
         quit()
 
 print('No change to database since last execution.')
-os.remove('now.csv')
-now_csv.close()
-prev_hashfile.close()
+os.remove('current.csv')
+current_csv.close()
+previous_hashfile.close()
